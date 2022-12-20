d5c580ed6129abd11a6b3bbc87eb2c02035835b3
Abdul Ahad <abdulahad.mohamed06@gmail.com>
2022-12-20 14:04:39 +0530

    design home and add products page

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/products',(req,res)=> {
-    res.send('All Products')
+router.get('/',(req,res)=> {
+    res.render('index', { title : 'home page'})
+})
+
+router.get("/add", (req,res)=>{
+    res.render("add_products", { title: "Add products"})
 })
 
 module.exports = router;
