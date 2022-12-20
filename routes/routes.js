b26ec9856476cb7c5a92a372cae84926008963b1
Abdul Ahad <abdulahad.mohamed06@gmail.com>
2022-12-20 14:57:17 +0530

    insert record into databse and display the records from db in home page

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,53 @@
 const express = require('express')
 const router = express.Router()
+const Product = require('../models/products')
+const multer = require('multer')
+
+//image upload
+var storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null, './uploads')
+    },
+    filename: function(req,file,cb){
+        cb(null, file.fieldname + "_" + file.originalname)
+    },
+})
+
+var upload = multer({
+    storage: storage,
+}).single('image');
+
+router.post('/add', upload, (req,res)=>{
+    const product = new Product({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        image: req.file.filename,
+    })
+    product.save((err)=>{
+        if(err){
+            res.json({message: err.message, type: 'danger'})
+        } else {
+            req.session.message ={
+                type: 'success',
+                message: 'Product added succeddfully'
+            };
+            res.redirect('/')
+        }
+    })
+})
 
 router.get('/',(req,res)=> {
-    res.render('index', { title : 'home page'})
+    Product.find().exec((err,products)=>{
+        if(err){
+            res.json({ message: err.message })
+        } else {
+            res.render('index', {
+                title: 'Home page',
+                products: products,
+            })
+        }
+    })
 })
 
 router.get("/add", (req,res)=>{
